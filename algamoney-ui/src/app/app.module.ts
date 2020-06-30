17c18a99312b45d96d1c077ea26f9dd86e621eb2
americoafonso <americo.quim@hotmail.com>
2020-06-30 00:57:56 -0300

    Implementation of the list of launches.
    Implementation of the list of launches.

--- algamoney-ui/src/app/app.module.ts
+++ algamoney-ui/src/app/app.module.ts
@@ -1,3 +1,5 @@
+import { LancamentoService } from './lancamentos/lancamento.service';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
@@ -42,12 +44,13 @@ import { AppComponent } from './app.component';
     CurrencyMaskModule,
     InputMaskModule,
     KeyFilterModule,
+    HttpClientModule,
 
     LancamentosModule,
     PessoasModule,
     CoreModule
   ],
-  providers: [],
+  providers: [LancamentoService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- algamoney-ui/src/app/lancamentos/lancamentos-pesquisa/lancamentos-pesquisa.component.ts
+++ algamoney-ui/src/app/lancamentos/lancamentos-pesquisa/lancamentos-pesquisa.component.ts
@@ -1,3 +1,4 @@
+import { LancamentoService } from './../lancamento.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,40 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LancamentosPesquisaComponent implements OnInit {
 
-  lancamentos = [
-    {
-      tipo: 'DESPESA', descricao: 'Compra de pão', dataVencimento: new Date(2017, 5, 30),
-      dataPagamento: null, valor: 4.55, pessoa: 'Padaria do José'
-    },
-    {
-      tipo: 'RECEITA', descricao: 'Venda de software', dataVencimento: new Date(2017, 6, 10),
-      dataPagamento: '09/06/2017', valor: 80000, pessoa: 'Atacado Brasil'
-    },
-    {
-      tipo: 'DESPESA', descricao: 'Impostos', dataVencimento: new Date(2017, 7, 20),
-      dataPagamento: null, valor: 14312, pessoa: 'Ministério da Fazenda'
-    },
-    {
-      tipo: 'DESPESA', descricao: 'Mensalidade de escola', dataVencimento: new Date(2017, 6, 5),
-      dataPagamento: '30/05/2017', valor: 800, pessoa: 'Escola Abelha Rainha'
-    },
-    {
-      tipo: 'RECEITA', descricao: 'Venda de carro', dataVencimento: new Date(2017, 8, 18),
-      dataPagamento: null, valor: 55000, pessoa: 'Sebastião Souza'
-    },
-    {
-      tipo: 'DESPESA', descricao: 'Aluguel', dataVencimento: new Date(2017, 7, 10),
-      dataPagamento: '09/07/2017', valor: 1750, pessoa: 'Casa Nova Imóveis'
-    },
-    {
-      tipo: 'DESPESA', descricao: 'Mensalidade musculação', dataVencimento: new Date(2017, 7, 13),
-      dataPagamento: null, valor: 180, pessoa: 'Academia Top'
-    }
-  ];
+  lancamentos = [];
 
-  constructor() { }
+  constructor(private lancamentoSrvice: LancamentoService) { }
 
   ngOnInit() {
+    this.pesquisar();
+  }
+
+  pesquisar() {
+    this.lancamentoSrvice.pesquisar()
+      .then(lancamentos => this.lancamentos = lancamentos);
   }
 
 }
